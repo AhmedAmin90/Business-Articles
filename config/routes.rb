50c197d6db800477df59ebf6eb03260921a8caca
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-09 14:17:36 +0300

    Add the main congiguration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :articles
-  devise_for :users
+  resources :articles 
+  devise_for :users , :controllers => { registrations: 'registrations' }
   resources :users 
   resources :articles do
     resources :votes , only: [:create , :destroy]
   end
+  resources :categories 
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
