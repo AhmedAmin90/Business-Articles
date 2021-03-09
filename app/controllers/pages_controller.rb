bba9777efd91d20fa0dc40fc099addd4e5cb72ac
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-09 20:40:16 +0300

    Add homepage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+    def home 
+        @articles = Article.all
+        @categories = Category.all
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resources :votes , only: [:create , :destroy]
   end
   resources :categories 
-  root to: 'articles#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
