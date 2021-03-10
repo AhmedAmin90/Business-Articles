2dbf8a774a494c6447698cc2200b62cd9f8b0cae
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-11 01:37:05 +0300

    Add basic show page of the categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,14 @@
 class CategoriesController < ApplicationController
     def index
+        @categories = Category.all
         @articles = Article.joins(:categories).merge(Category.order(priority: :asc)).limit(4)
         @feature_article = Article.joins(:votes).merge(Vote.order(article_id: :desc)).first
 
+    end
+
+    def show 
+      @category = Category.find(params[:id])
+      @articles = Article.includes(:categories).where(categories: { name: @category.name })
     end
 
     def new
