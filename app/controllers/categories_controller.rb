da1780781779a78e1fc83035d603a3e0917db288
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-11 22:25:55 +0300

    Modify routes and controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,25 +10,4 @@ class CategoriesController < ApplicationController
       @category = Category.find(params[:id])
       @articles = Article.includes(:categories).where(categories: { name: @category.name })
     end
-
-    def new
-        @category = Category.new
-    end
-
-    def create
-      @category = Category.new(category_params)
-  
-        if @category.save
-         redirect_to categories_path, notice: "Category was successfully created." 
-        else
-          render :new
-        end
-    end
-
-      private
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name, :priority)
-    end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles 
+  resources :articles ,  except: [:index]
   devise_for :users , :controllers => { registrations: 'registrations' }
-  resources :users 
-  resources :articles do
+  resources :articles ,  except: [:index] do
     resources :votes , only: [:create , :destroy]
   end
-  resources :categories 
+  resources :categories , only: [:show , :index]
   root to: 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
