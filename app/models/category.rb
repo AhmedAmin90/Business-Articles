fb4b25af12804d0510f9665e337bd61e7768e4c8
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-11 14:27:17 +0300

    Add category links on header

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
     has_many :articles_categories
     has_many :articles , through: :articles_categories
+
+    scope :entrepreneurship, -> { where(:name => "entrepreneurship")}
+    scope :economy, -> { where(:name => "Economy")}
+    scope :technology, -> { where(:name => "Technology")}
+    scope :marketing, -> { where(:name => "marketing")}
+    # Ex:- scope :active, -> {where(:active => true)}
 end
