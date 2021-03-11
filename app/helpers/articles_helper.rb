70a410ffdbd51db054d19568c39d46f7dfe6c95e
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-11 21:36:57 +0300

    Add helper for home page , update vote method

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+    def edit_articles(article)
+        if user_signed_in? && current_user.id == article.author_id 
+            list = "#{ link_to 'Edit The Article', edit_article_path(@article) , class: "btn btn-secondary"} "
+             else
+            list = "#{ link_to 'Back To Home page', root_path , class: "btn btn-secondary"} "
+           end 
+        list.html_safe
+    end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,5 @@
 module CategoriesHelper
-    def show_category_page(articles)
+    def archive_category_page(articles)
         
         list = '<div class= "row">'
         articles.each do |article|
@@ -15,7 +15,24 @@ module CategoriesHelper
             list += " <h6 class='card-subtitle mb-2 text-muted'> #{article.title} </h6>"
             list += " <p class='card-text'> #{article.text.slice(0..200)}... </p>"
             list += "<p class='btn btn-warning m-1'>  #{vote_or_unvote_btn(article)} </p>"
-            list += "<p class='btn bg-orange m-1'>  #{article.votes.count} </p>"
+            list += "<p class='btn bg-orange m-1'> Votes: #{article.votes.count} </p>"
+            list += '</div>'
+            list += '</div>'
+            list += '</div>'
+        end
+        list += '</div>'
+        list.html_safe
+    end
+
+    def four_cards(articles)
+        list = '<div class= "row">'
+        articles.each do |article|
+            list += ' <div class="col-3 p-0 ">'
+            list += '<div class="card bg-dark text-white border-0">'
+            list += "#{image_tag  article.image , class: "card-img feature-img"}  "
+            list += '<div class="card-img-overlay card-article-content d-flex flex-column justify-content-between">'
+            list += "<p class='card-text text-dark'> #{link_to article.categories.first.name , category_path(article.categories.first.id) , class: "card-article-category"} </p>"
+            list += "<h5 class='card-title text-dark '> #{ link_to  article.title , article_path(article) , class: "card-article-title" }</h5>"
             list += '</div>'
             list += '</div>'
             list += '</div>'
