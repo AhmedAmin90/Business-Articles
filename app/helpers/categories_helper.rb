705dfb601420fa476fd1d80c6689bad017b39fe8
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-12 19:28:47 +0300

    Update archive method to reverse columns

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,8 +1,27 @@
 module CategoriesHelper
+
     def archive_category_page(articles)
+        group_one = []
+        group_two = []
+        articles_one = []
+        articles_two = []
+        articles.each_with_index do |article , i|
+            group_one <<  article if i % 2 == 0 
+            group_two << article if i % 2 != 0
+        end
+        group_one.each_with_index do |article , i|
+            articles_one << article if i % 2 == 0
+            articles_two << article if i % 2 != 0 
+        end
+        group_two.each_with_index do |num , i|
+            articles_one << num if i % 2 == 0
+            articles_two << num if i % 2 != 0 
+        end
         
         list = '<div class= "row">'
+        
         articles.each do |article|
+          if articles_one.include?(article)
             list += "<div class='col-3 p-0'> "
             list += '<div class="card bg-dark text-white border-0"> '
             list += "#{image_tag  article.image , height: '300px'}  "
@@ -19,6 +38,24 @@ module CategoriesHelper
             list += '</div>'
             list += '</div>'
             list += '</div>'
+          else
+            list += '<div class="col-3 p-0">'
+            list += '<div class="card details-card border-0">'
+            list += '<div class="card-body">'
+            list += " <h5 class='card-title'> #{article.categories.first.name}</h5>"
+            list += " <h6 class='card-subtitle mb-2 text-muted'> #{article.title} </h6>"
+            list += " <p class='card-text'> #{article.text.slice(0..200)}... </p>"
+            list += "<p class='btn btn-warning m-1'>  #{vote_or_unvote_btn(article)} </p>"
+            list += "<p class='btn bg-orange m-1'> Votes: #{article.votes.count} </p>"
+            list += '</div>'
+            list += '</div>'
+            list += '</div>'
+            list += "<div class='col-3 p-0'> "
+            list += '<div class="card bg-dark text-white border-0"> '
+            list += "#{image_tag  article.image , height: '300px'}  "
+            list += '</div>'
+            list += '</div>'
+          end 
         end
         list += '</div>'
         list.html_safe
@@ -42,20 +79,3 @@ module CategoriesHelper
         list.html_safe
     end
 end
-
-# def four_cards(articles)
-#     list = '<div class= "row">'
-#     articles.each do |article|
-#         list += ' <div class="col-3 p-0 ">'
-#         list += '<div class="card bg-dark text-white border-0">'
-#         list += "#{image_tag  article.image , class: "card-img feature-img"}  "
-#         list += '<div class="card-img-overlay card-article-content d-flex flex-column justify-content-between">'
-#         list += "<p class='card-text text-dark'> #{link_to article.categories.first.name , category_path(article.categories.first.id) , class: "card-article-category"} </p>"
-#         list += "<h5 class='card-title text-dark '> #{ link_to  article.title , article_path(article) , class: "card-article-title" }</h5>"
-#         list += '</div>'
-#         list += '</div>'
-#         list += '</div>'
-#     end
-#     list += '</div>'
-#     list.html_safe
-# end
