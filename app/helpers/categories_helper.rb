92d5ab411220916372186fe185589d4a6c2b9690
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-15 19:50:54 +0300

    Add author name

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -32,26 +32,25 @@ module CategoriesHelper
         list += '<div class="card-body ">'
         list += " <h5 class='card-title text-yellow'> #{article.categories.first.name}</h5>"
         list += " <h6 class='card-subtitle mb-2  text-gray fw-bold'> #{article.title} </h6>"
-        list += " <p class='card-text'> #{article.text.slice(0..200)}... </p>"
-        list += "<p class='btn btn-warning mx-1 mt-2'>  #{vote_or_unvote_btn(article)} </p>"
-        list += "<p class='btn bg-orange mx-1 mt-2'> Votes: #{article.votes.count} </p> </div> </div> </div>"
-
+        list += " <p class='card-subtitle text-warning fw-bold'> Author: #{article.author.name} </p>"
+        list += " <p class='card-text'> #{article.text.slice(0..150)}... </p>"
+        list += "<p class='btn btn-warning mx-1 mt-2'>  #{vote_or_unvote_btn(article)} </p> <p class='btn bg-orange mx-1 mt-2'> Votes: #{article.votes.count} </p> </div> </div> </div>"
+        
       else
         list += '<div class="col-3 p-0">'
         list += '<div class="card details-card border-0 rounded-0">'
         list += '<div class="card-body">'
         list += " <h5 class='card-title'> #{article.categories.first.name}</h5>"
         list += " <h6 class='card-subtitle mb-2 text-muted'> #{article.title} </h6>"
+        list += " <p class='card-subtitle   text-warning fw-bold'> Author: #{article.author.name} </p>"
         list += " <p class='card-text'> #{article.text.slice(0..200)}... </p>"
-        list += "<p class='btn btn-warning m-1'>  #{vote_or_unvote_btn(article)} </p>"
-        list += "<p class='btn bg-orange m-1'> Votes: #{article.votes.count} </p>"
+        list += "<p class='btn btn-warning m-1'>  #{vote_or_unvote_btn(article)} </p> <p class='btn bg-orange m-1'> Votes: #{article.votes.count} </p>"
         list += '</div>'
         list += '</div>'
         list += '</div>'
         list += "<div class='col-3 p-0'> "
         list += '<div class="card bg-dark text-white border-0"> '
         list += "#{image_tag article.image, height: '300px'}  </div> </div>"
-
       end
     end
     list += '</div>'
