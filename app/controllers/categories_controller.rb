4608953def05c55131558fdea0f6c237cc7486b3
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-15 20:00:06 +0300

    include author in articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,11 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.includes(:articles).order(priority: :asc)
-    @articles = Article.includes(:categories)
+    @articles = Article.includes(:categories , :votes)
   end
 
   def show
     @category = Category.find(params[:id])
-    @articles = Article.includes(:categories).where(categories: { name: @category.name })
+    @articles = Article.includes(:categories, :author , :votes).where(categories: { name: @category.name })
   end
 end
