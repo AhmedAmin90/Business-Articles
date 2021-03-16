0629831a6001ef806653dd1f5424bfcedfe331c3
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-16 11:05:30 +0300

    Order articles from the most recent to the oldest one.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @articles = Article.includes(:categories, :author, :votes).where(categories: { name: @category.name })
+    @articles = Article.includes(:categories, :author, :votes).where(categories: { name: @category.name }).order(created_at: :desc)
   end
 end
